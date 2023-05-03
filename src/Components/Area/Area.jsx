a79e617c44788e3526142d51d0ff94daec9ae2bf
Mohammed uzair mauzam <uzairmauzam68@gmail.com>
2023-05-03 12:51:30 +0530

    area done

---
+++ src/Components/Area/Area.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Area.css'
+
+function Area() {
+  return (
+    <div className='main-div'>
+        <div className="leftarea">
+            <p className='intro'>HEY! Myself</p>
+            <h1><span>Mohammed</span> Uzair Mauzam</h1>
+            <h3><span>W</span>eb Developer</h3>
+            <p className='des'> I'm a front end developer with more than 2 years of experience in web development</p>
+
+        </div>
+        <div className="rightarea">
+            <img src="/images/developer1.png" alt="" />
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default Area
